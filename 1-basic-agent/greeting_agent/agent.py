e9341d950c02578a730ce761618b70f6615db938
mrme77 <psalomone33@gmail.com>
2025-05-23 22:41:00 -0500

    edits

--- 1-basic-agent/greeting_agent/agent.py
+++ 1-basic-agent/greeting_agent/agent.py
@@ -1,28 +1,28 @@
-# from google.adk.agents import Agent
-
-# root_agent = Agent(
-#     name="greeting_agent",
-#     # https://ai.google.dev/gemini-api/docs/models
-#     model="gemini-2.0-flash",
-#     description="Greeting agent",
-#     instruction="""
-#     You are a helpful assistant that greets the user. 
-#     Ask for the user's name and greet them by name and also say 
-#     Napoli won his 4th scudetto!
-#     """)
-
 from google.adk.agents import Agent
 
-# Step 1: Define the agent
 root_agent = Agent(
     name="greeting_agent",
-    model="gemma3",  # Use the Gemma/Gemini model
+    # https://ai.google.dev/gemini-api/docs/models
+    model="gemini-2.0-flash",
     description="Greeting agent",
     instruction="""
     You are a helpful assistant that greets the user. 
-    Ask for the user's name and greet them by name and add Forza Napoli!
-    """
-)
+    Ask for the user's name and greet them by name and also say 
+    Napoli won his 4th scudetto!
+    """)
+
+# from google.adk.agents import Agent
+
+# # Step 1: Define the agent
+# root_agent = Agent(
+#     name="greeting_agent",
+#     model="gemma3",  # Use the Gemma/Gemini model
+#     description="Greeting agent",
+#     instruction="""
+#     You are a helpful assistant that greets the user. 
+#     Ask for the user's name and greet them by name and add Forza Napoli!
+#     """
+# )
 
 
 
