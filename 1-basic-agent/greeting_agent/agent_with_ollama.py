a3874e04422713994480117f26a2d5a4d5dfce06
mrme77 <psalomone33@gmail.com>
2025-05-23 22:00:07 -0500

    edits

---
+++ 1-basic-agent/greeting_agent/agent_with_ollama.py
@@ -0,0 +1,14 @@
+from google.adk.agents import Agent
+
+# Step 1: Define the agent
+root_agent = Agent(
+    name="greeting_agent",
+    model="gemma3:12b-it-qat",  # Use the Gemma/Gemini model
+    description="Greeting agent",
+    instruction="""
+    You are a helpful assistant that greets the user. 
+    Ask for the user's name and greet them by name and add Forza Napoli!
+    """,
+)
+
+
